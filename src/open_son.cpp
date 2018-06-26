9ce8b642322585a0a675144592d39b1331343c42
Paicaloid <paicaloid@gmail.com>
2018-06-26 15:30:34 +0700

    Add comment

--- src/open_son.cpp
+++ src/open_son.cpp
@@ -21,10 +21,9 @@ int bottom = 0 ;
 float start_range = 1 ;
 float stop_range = 20 ;
 
-int thres = 0 ;
 double maxval = 255 ;
-int thres_type = CV_THRESH_BINARY ;
 
+int thres_type = CV_THRESH_BINARY ;
 int interpolation = CV_INTER_LINEAR ;
 
 BVTSDK::Sonar sonar;
@@ -32,28 +31,29 @@ BVTSDK::Head head = NULL ;
 BVTSDK::Ping ping = NULL ;
 BVTSDK::ImageGenerator img;
 BVTSDK::ColorMapper map;
-cv::Mat color_img ;
-cv::Mat binary_img ;
+BVTSDK::ColorMapper automap;
+// cv::Mat color_img ;
+// cv::Mat auto_img ;
 
+/// Changing Gamma in ColorMapper ///
 void Set_Gamma( int , void* ){
 	float int2float = set_gamma/100.0 ;
 	map.SetGamma(int2float) ;
 }
 
+/// Changing Threshold in ColorMapper ///
 void SetMapperThreshold( int , void*){
 	if(top > bottom){
 		map.SetThresholds(top, bottom);
 	}
 }
 
+/// Changing Sound speed in ImageGenerator ///
 void SpeedChange( int , void*){
 	img.SetSoundSpeedOverride(pos);
 }
 
-void ThresholdChange( int , void*){
-	threshold(binary_img, binary_img,thres, maxval, cv::THRESH_BINARY );
-}
-
+/// Show Sonar information ///
 void Sonar_status(){
 	std::string model = sonar.GetSonarModelName() ;
 	int fileSize = sonar.GetFileSize() ;
@@ -91,178 +91,129 @@ void Sonar_status(){
 int main(int argc, char** argv){
 
 	/// Setup Path ///
-	std::string rootPath = "/home/paicaloid/bvtsdk/";
-	std::string dataPath = rootPath + "data/";
-	std::string mapperPath = rootPath + "colormaps/bone.cmap";
-	std::string fileName = "Apr_27_2017_12842_6.son";
+	std::string rootPath = "/home/paicaloid/bvtsdk/";			// SDK path
+	std::string dataPath = rootPath + "data/";					// .son path
+	std::string mapperPath = rootPath + "colormaps/bone.cmap";	// Colormapper path
+	std::string fileName = "Apr_27_2017_12842_6.son";			// filename of .son
 	std::string fullPath = dataPath + fileName;
 
-	pos = 1530;
-	intensity = 30;
-	thres = 70 ;
-
+	/// Create Windowname ///
 	cv::namedWindow("RawImage" , cv::WINDOW_NORMAL);
-	//cv::namedWindow("BinaryThresholding" , cv::WINDOW_NORMAL);
 	cv::namedWindow("RThetaImage", cv::WINDOW_NORMAL );
-	cv::createTrackbar("Sound Speed" , "RawImage" , &pos , 2000 , SpeedChange);
-	//cv::createTrackbar("Threshold" , "BinaryThresholding" , &thres , 255 , ThresholdChange);
-	cv::createTrackbar("Sound Speed" , "RThetaImage" , &pos , 2000 , SpeedChange);
-
-	sonar.Open("FILE" , fullPath);
-	head = sonar.GetHead(0);
-	head.SetRange(start_range, stop_range) ;
-	ping = head.GetPing(0);
+	cv::namedWindow("RawImage_Auto", cv::WINDOW_NORMAL );
+	
+	// Open .son file ///
+	sonar.Open("FILE" , fullPath);				// Open .son file
+	head = sonar.GetHead(0);					// Connect head to sonar, 0 = single-head sonar
+	head.SetRange(start_range, stop_range) ;	// Setup range 
+	ping = head.GetPing(0);						// Connnect Ping to head with specific ping (Ping #0 or first ping)
+	img.SetHead(head);							// Create ImageGenerator
+	map.Load(mapperPath);						// Load Colormapper
+	map.SetAutoMode(0);							// Disable Auto Parameter, 0 = disable
+	automap.Load(mapperPath) ;					// Load Colormapper
+	automap.SetAutoMode(1) ;					// Enable Auto Parameter, 1 = enable 
 	Sonar_status() ;
 
-	img.SetHead(head);
-	float resolution = img.GetRangeResolution() ;
-	printf("Resolution : %f meters\n", resolution*100000) ;
-	float xx = img.GetClippingThreshold();
-	float yy = img.GetNoiseThreshold() ;
-	printf("===============> %f %f\n", xx, yy) ;
-	// printf("===============> %f &f\n", xx, yy) ;
-	
-	map.Load(mapperPath);
-
-	map.SetAutoMode(1);
-
-	if(map.GetAutoMode())
-	{
-		printf("AutoMode : Enable\n");
-	}
-	else
-	{
-		printf("AutoMode : Disable\n");
-	}
-
+	/// Create Trackbar ///
+	pos = 1530;
 	top = map.GetTopThreshold() ;
 	bottom = map.GetBottomThreshold() ;
 	ggg = map.GetGamma() ;
-
-	printf("Gamma : %f\n", ggg) ;
-	printf("Top : %d\n", top) ;
-	printf("Bottom : %d\n", bottom) ;
-
-	// int top = 15000 ;
-	// int bottom = 10000 ;
-	
-	// map.SetThresholds(top, bottom) ;
-
+	/// RawImage Trackerbar ///
+	cv::createTrackbar("Sound Speed" , "RawImage" , &pos , 2000 , SpeedChange);
 	cv::createTrackbar("Top Thres" , "RawImage" , &top , 32700 , SetMapperThreshold);
 	cv::createTrackbar("Bottom Thres" , "RawImage" , &bottom , 32700 , SetMapperThreshold);
 	cv::createTrackbar("Gamma" , "RawImage" , &set_gamma , 100 , Set_Gamma);
+	/// Soundspeed Trackbar ///
+	cv::createTrackbar("Sound Speed" , "RThetaImage" , &pos , 2000 , SpeedChange);
+	cv::createTrackbar("Sound Speed" , "RawImage_Auto" , &pos , 2000 , SpeedChange);
+	
 
 	int p = 1;
 	int k = 0;
 	int j = 0 ;
 
+	/// ROS Initial ///
 	printf("Initial Publisher for /imaging_sonar\n") ;
 	printf("Pause video by pressing : P\n") ;
 	ros::init(argc, argv, "image_publisher");
 	ros::NodeHandle nh;
 	image_transport::ImageTransport it(nh);
 	image_transport::Publisher pub = it.advertise("/imaging_sonar", 1);
-	
-
 	ros::Rate loop_rate(5);
+
 
 	int pai = 0 ;
 
 	while (nh.ok()) {
-		//map.Load(mapperPath);
 
-		// float Gamma = 0.5 ;
-		// map.SetGamma(Gamma) ;
+		ping = head.GetPing(-1);  	// Getping less than 0 is get next ping
 
+		/// Genarate XY ColorImage ///
+		BVTSDK::MagImage mag = img.GetImageXY(ping);			// Get XY image from ping
+		BVTSDK::ColorImage cimg = map.MapImage(mag);			// Covert to ColorImage with Colormapping
+		BVTSDK::ColorImage cimg_auto = automap.MapImage(mag);	// Covert to ColorImage with Colormapping
+
+		/// Generate RTheta ColorImage ///
+		BVTSDK::MagImage rth = img.GetImageRTheta(ping);		// Get RTheta image from ping
+		BVTSDK::ColorImage cimgrth = map.MapImage(rth);			// Covert to ColorImage with Colormapping
 		
-
-		ping = head.GetPing(-1);
-		BVTSDK::MagImage mag = img.GetImageXY(ping);
-		BVTSDK::MagImage rth = img.GetImageRTheta(ping);
-		BVTSDK::ColorImage cimg = map.MapImage(mag);
-		BVTSDK::ColorImage cimgrth = map.MapImage(rth) ;
-
-		// float ClipThres = 1.0 ;
-		// img.SetClippingThreshold(ClipThres) ;
-		
-		// pai++ ;
-		// if(pai == 10 ){
-		// 	pai = 0 ;
-		// 	clip = clip + 100 ;
-		// 	head.SetClippingThreshold(clip) ;
-		// 	printf("Clipping : %d\n", head.GetClippingThreshold()) ;
-		// }
-
-		//Gamma = map.GetGamma() ;
-		//printf("Colormap Gamma : %f\n",Gamma) ;
-		
+		/// Find XY Image resolution ///
 		int height = cimg.GetHeight();
 		int width = cimg.GetWidth();
+		printf("XY Image resolution : %d x %d\n", width, height) ;
 
+		/// Find RTheta Image resolution ///
 		int height_rth = cimgrth.GetHeight();
 		int width_rth = cimgrth.GetWidth();
-
-		//printf("Height : %d, Width : %d\n",height, width) ;
+		printf("RTheta Image resolution : %d x %d\n", width_rth, height_rth) ;
 		
-		// unsigned int *bits = cimg.GetBits() ;
-		// printf("%u\n", bits) ;
-		
+		/// Convert to Matrix format ///
+		/// GetBits() will return pointer of the entrie image ///
 		cv::Mat color_img(height , width , CV_8UC4 , cimg.GetBits());
-		cv::Mat binary_img(height , width , CV_8UC4 , cimg.GetBits());
+		cv::Mat auto_img(height , width , CV_8UC4 , cimg_auto.GetBits());
 		cv::Mat color_rth(height_rth , width_rth , CV_8UC4 , cimgrth.GetBits());
 
-		// Pause by pressing 'P'
+		// Pause by pressing 'P' ///
 	 	while(!p){
 	 		k = cv::waitKey(30);
 	    	if (k == 112){
-				// printf("ClipThres : %f\n", ClipThres) ;
 	    		p = !p;
 	    	}
 		}
-		//double thres = 70 ;
-		//double maxval = 255 ;
-		//int thres_type = CV_THRESH_BINARY ;
+		
+		/// Chgange Image to grayscale ///
 		cvtColor(color_img, color_img, cv::COLOR_RGB2GRAY);
+		cvtColor(auto_img, auto_img, cv::COLOR_RGB2GRAY);
 		cvtColor(color_rth, color_rth, cv::COLOR_RGB2GRAY);
-		//cvtColor(binary_img, binary_img, cv::COLOR_RGB2GRAY);
-		//threshold(binary_img, binary_img,thres, maxval, cv::THRESH_BINARY );
-		
-		
+
+		/// Show Image ///
 		cv::imshow("RawImage", color_img);
-		//cv::imshow("BinaryThresholding", binary_img) ;
+		cv::imshow("RawImage_Auto", auto_img) ;
 		cv::imshow("RThetaImage", color_rth) ;
-        k = cv::waitKey(30);
+        
+		/// unknown ///
+		k = cv::waitKey(30);
     	if( k == 27 )
     		break;
     	else if (k == 112){
     		p = !p;
     	}
 
-		int row = color_img.rows ;
-		int col = color_img.cols ;
-		//printf("Row : %d, Column : %d\n",row, col) ;
+		// int row = color_img.rows ;
+		// int col = color_img.cols ;
 
-		//resize image
+		/// Resize Image for sending through ROS ///
 		cv::resize(color_img, color_img, cv::Size(), 0.5, 0.5, interpolation) ;
 		cv::resize(color_rth, color_rth, cv::Size(), 0.5, 0.5, interpolation) ;
 		int roww = color_img.rows ;
 		int coll = color_img.cols ;
-		//printf("Row : %d, Column : %d\n",roww, coll) ;
 
-		// j++ ;
-		// if(j == PingCount)
-		// {
-		// 	printf("\n\n\n\n\n\n ### Finish ### \n\n\n\n\n\n") ;
-		// 	j = 0 ;
-		// }
-
-		//printf("Sending Image\n") ;
+		/// ROS Message ///
 		sensor_msgs::ImagePtr msg = cv_bridge::CvImage(std_msgs::Header(), "mono8", color_rth).toImageMsg();
 		pub.publish(msg);
 		ros::spinOnce();
 		loop_rate.sleep();
-
-		
 
 	}
 }
